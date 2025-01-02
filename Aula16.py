7ffa20cfae8b83c0353edba9be5fbb96225d141c
Vânio Anibal  Macamo <macamovanioanibal@gmail.com>
2025-01-02 17:25:20 +0200

    Aulas até interpolação de Strings

--- Aula16.py
+++ Aula16.py
@@ -1,3 +1,12 @@
 
 #if / elif / else
 
+entrada = input('Você quer "entrar" ou "Sair"?')
+
+
+if entrada == 'entrar':
+    print('Você entrou no sistema.')
+elif entrada == 'sair':
+    print('Voce saiu do sistema.')
+else: 
+    print('Você não digitou nem entrar nem sair.')
