92f99c798b2c0bc428bc90f3af916dbf4dfe7a56
Vânio Anibal  Macamo <macamovanioanibal@gmail.com>
2025-01-03 20:02:53 +0200

    Funcionamento do ciclo for

---
+++ Aula43.py
@@ -0,0 +1,30 @@
+"""
+For + Range
+
+range -> range(start, stop, step)
+"""
+#Esse é um iteravel finito
+# numeros = range(0, 10, 1)
+
+# for numero in numeros:
+#     print(numero)
+
+# print(next(texto))
+# print(next(texto))
+# print(next(texto))
+# print(next(texto))
+# print(next(texto))
+# print(next(texto))
+
+texto = "Vanio" #Iteravel
+# iterador = iter(texto)  #iterator
+
+# while True:
+#     try:
+#         letra = next(iterador)
+#         print(letra)
+#     except StopIteration:
+#         break
+
+for letra in texto:
+    print(letra)
