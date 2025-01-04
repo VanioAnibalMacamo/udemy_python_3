b56d25ed37b6d245cfe550e570e62a6fb77ea6dc
Vânio Anibal  Macamo <macamovanioanibal@gmail.com>
2025-01-04 15:15:02 +0200

    Iniciando o estudo de listas.

---
+++ Aula48.py
@@ -0,0 +1,15 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+
+#print(list, type(list))
+# print(bool(lista))
+# print(bool([]))
+lista = [123, True, 'Vanio Macamo', 1.2, []]
+lista[2] = 'Luis'
+print(lista[2], type(lista[2]))
